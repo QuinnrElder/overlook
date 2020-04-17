503e0a2d68ec2c758e58556194dd81fb8dd066bb
Quinn Elder <qre.turing@gmail.com>
2020-04-17 17:46:50 -0600

    :art: Add CSS for the user page

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -12,28 +12,29 @@ const domUpdates = {
 
   injectManagerHTML() {
     $('.manager-page').html(`
-    <section class="main-navs'>
-      <p class="nav-title">As of Now...</p>
-      <section class="nav-box'>
-        <p class="all-available-rooms-txt">Available Rooms</p>
-        <p class="all-available-rooms">//</p>
+    <section class="manager-navs">
+        <p class="nav-title">The Hotel has:</p>
+        <section class="nav-box">
+          <p class="all-available-rooms-txt">Available Rooms</p>
+          <p class="all-available-rooms">//</p>
+        </section>
+        <section class="nav-box">
+          <p class="total-revenue-text">Total Revenue</p>
+          <p class="total-revenue">//</p>
+        </section>
+        <section class="nav-box">
+          <p class="available-percent-text">Available %</p>
+          <p class="available-percent">//</p>
+        </section>
       </section>
-      <section class="nav-box'>
-      <p class="total-revenue-text">Total Revenue</p>
-      <p class="total-revenue">//</p>
+      <section class="main-manager-info">
+        <p class="todays-date">//</p>
+        <section class="manager-search-nav">
+          <label class="search-nav-title">Customers Name?</label>
+          <input class="input-name" type="text"></input>
+          <input id="search-btn" type="submit">
+        </section>
       </section>
-      <section class="nav-box'>
-      <p class="available-percent-text">Available %</p>
-      <p class="available-percent">//</p>
-      </section>
-    </section>
-    <p class="todays-date"></p>
-    <section class="manager-search-nav">
-      <p class="search-nav-title">Who are we Helping?</p>
-      <input class="input-name" type="test"></input>
-      <input id="search-btn" type="submit">
-    </section>
-    <section class="search-window">//</section>
     `)
   },
 
@@ -61,7 +62,6 @@ const domUpdates = {
       <input class="filter-input" type=""></input>
       <input id="search-btn" type="submit">
     </section>
-    <section class="search-window">//</section>
     `)
   },
 
